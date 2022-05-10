e75d3074d9dc48b344abdd82d9f608ad1a1b7a5a
AntonGruenko <83608448+AntonGruenko@users.noreply.github.com>
2022-05-10 16:09:00 +0300

    statistics, water counting, databases added

---
+++ app/src/main/java/com/example/project/StartActivity.java
@@ -0,0 +1,24 @@
+package com.example.project;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StartActivity.this, HeightActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
